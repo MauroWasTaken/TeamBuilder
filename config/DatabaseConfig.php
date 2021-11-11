63ecd9eb65e217198c875c06a8b601ef6cf92283
maurxsantoz <maurx18@gmail.com>
2021-11-11 17:03:09 +0100

    Add UTF-8 support

---
+++ config/DatabaseConfig.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DSN", "mysql:host=localhost;dbname=teambuilder;charset=utf8");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("SQLPATH", dirname(__DIR__) . "/db/teambuilder.sql");
+define("AUTOLOGINID", 7);
